a46b3f0f4a59cddd2e6e947f01e0c8eeea7abad3
Simon He <57086651+Simon-He95@users.noreply.github.com>
2024-09-29 14:51:02 +0800

    chore: init

---
+++ src/tdesignVueNext1/index.ts
@@ -0,0 +1,21 @@
+import { getComponentsMap, getPropsMap } from './mapping'
+// import directives from '../directives.json'
+
+export function tdesignVueNext1() {
+  return {
+    uiName: 'tdesign-vue-next',
+    lib: 'tdesign-vue-next',
+    map: getPropsMap(),
+  }
+}
+
+export function tdesignVueNext1Components() {
+  return {
+    map: getComponentsMap(),
+    isSeperatorByHyphen: false,
+    prefix: 't',
+    isReact: false,
+    lib: 'tdesign-vue-next',
+    // directives: directives.tdesignMobileVue1,
+  }
+}
---
+++ src/tdesignVueNext1/mapping.ts
@@ -0,0 +1,325 @@
+import Affix from './Affix.json'
+import Alert from './Alert.json'
+import Anchor from './Anchor.json'
+import AnchorItem from './AnchorItem.json'
+import AnchorTarget from './AnchorTarget.json'
+import Aside from './Aside.json'
+import AutoComplete from './AutoComplete.json'
+import Avatar from './Avatar.json'
+import AvatarGroup from './AvatarGroup.json'
+import BackTop from './BackTop.json'
+import Badge from './Badge.json'
+import BaseTable from './BaseTable.json'
+import Breadcrumb from './Breadcrumb.json'
+import BreadcrumbItem from './BreadcrumbItem.json'
+import Button from './Button.json'
+import Calendar from './Calendar.json'
+import Card from './Card.json'
+import Cascader from './Cascader.json'
+import CheckTag from './CheckTag.json'
+import CheckTagGroup from './CheckTagGroup.json'
+import Checkbox from './Checkbox.json'
+import CheckboxGroup from './CheckboxGroup.json'
+import Col from './Col.json'
+import Collapse from './Collapse.json'
+import CollapsePanel from './CollapsePanel.json'
+import ColorPicker from './ColorPicker.json'
+import Comment from './Comment.json'
+import DatePicker from './DatePicker.json'
+import DatePickerPanel from './DatePickerPanel.json'
+import DateRangePicker from './DateRangePicker.json'
+import DateRangePickerPanel from './DateRangePickerPanel.json'
+import DescriptionItem from './DescriptionItem.json'
+import Descriptions from './Descriptions.json'
+import Dialog from './Dialog.json'
+import Divider from './Divider.json'
+import Drawer from './Drawer.json'
+import Dropdown from './Dropdown.json'
+import DropdownItem from './DropdownItem.json'
+import DropdownMenu from './DropdownMenu.json'
+import Empty from './Empty.json'
+import EnhancedTable from './EnhancedTable.json'
+import Footer from './Footer.json'
+import Form from './Form.json'
+import FormItem from './FormItem.json'
+import Guide from './Guide.json'
+import HeadMenu from './HeadMenu.json'
+import Header from './Header.json'
+import Image from './Image.json'
+import ImageViewer from './ImageViewer.json'
+import Input from './Input.json'
+import InputAdornment from './InputAdornment.json'
+import InputGroup from './InputGroup.json'
+import InputNumber from './InputNumber.json'
+import Layout from './Layout.json'
+import Link from './Link.json'
+import List from './List.json'
+import ListItem from './ListItem.json'
+import ListItemMeta from './ListItemMeta.json'
+import Loading from './Loading.json'
+import Menu from './Menu.json'
+import MenuGroup from './MenuGroup.json'
+import MenuItem from './MenuItem.json'
+import Message from './Message.json'
+import Notification from './Notification.json'
+import Option from './Option.json'
+import OptionGroup from './OptionGroup.json'
+import Pagination from './Pagination.json'
+import PaginationMini from './PaginationMini.json'
+import Popconfirm from './Popconfirm.json'
+import Popup from './Popup.json'
+import PrimaryTable from './PrimaryTable.json'
+import Progress from './Progress.json'
+import Radio from './Radio.json'
+import RadioGroup from './RadioGroup.json'
+import RangeInput from './RangeInput.json'
+import RangeInputPopup from './RangeInputPopup.json'
+import Rate from './Rate.json'
+import Row from './Row.json'
+import Select from './Select.json'
+import SelectInput from './SelectInput.json'
+import Skeleton from './Skeleton.json'
+import Slider from './Slider.json'
+import Space from './Space.json'
+import Statistic from './Statistic.json'
+import StepItem from './StepItem.json'
+import Steps from './Steps.json'
+import StickyItem from './StickyItem.json'
+import StickyTool from './StickyTool.json'
+import Submenu from './Submenu.json'
+import Swiper from './Swiper.json'
+import Switch from './Switch.json'
+import Tag from './Tag.json'
+import TagInput from './TagInput.json'
+import Textarea from './Textarea.json'
+import TimePicker from './TimePicker.json'
+import TimeRangePicker from './TimeRangePicker.json'
+import Timeline from './Timeline.json'
+import TimelineItem from './TimelineItem.json'
+import Tooltip from './Tooltip.json'
+import TooltipLite from './TooltipLite.json'
+import Transfer from './Transfer.json'
+import Tree from './Tree.json'
+import TreeSelect from './TreeSelect.json'
+import Upload from './Upload.json'
+import Watermark from './Watermark.json'
+
+export function getPropsMap() {
+  return [
+    Affix,
+    Alert,
+    Anchor,
+    AnchorItem,
+    AnchorTarget,
+    Aside,
+    AutoComplete,
+    Avatar,
+    AvatarGroup,
+    BackTop,
+    Badge,
+    BaseTable,
+    Breadcrumb,
+    BreadcrumbItem,
+    Button,
+    Calendar,
+    Card,
+    Cascader,
+    CheckTag,
+    CheckTagGroup,
+    Checkbox,
+    CheckboxGroup,
+    Col,
+    Collapse,
+    CollapsePanel,
+    ColorPicker,
+    Comment,
+    DatePicker,
+    DatePickerPanel,
+    DateRangePicker,
+    DateRangePickerPanel,
+    DescriptionItem,
+    Descriptions,
+    Dialog,
+    Divider,
+    Drawer,
+    Dropdown,
+    DropdownItem,
+    DropdownMenu,
+    Empty,
+    EnhancedTable,
+    Footer,
+    Form,
+    FormItem,
+    Guide,
+    HeadMenu,
+    Header,
+    Image,
+    ImageViewer,
+    Input,
+    InputAdornment,
+    InputGroup,
+    InputNumber,
+    Layout,
+    Link,
+    List,
+    ListItem,
+    ListItemMeta,
+    Loading,
+    Menu,
+    MenuGroup,
+    MenuItem,
+    Message,
+    Notification,
+    Option,
+    OptionGroup,
+    Pagination,
+    PaginationMini,
+    Popconfirm,
+    Popup,
+    PrimaryTable,
+    Progress,
+    Radio,
+    RadioGroup,
+    RangeInput,
+    RangeInputPopup,
+    Rate,
+    Row,
+    Select,
+    SelectInput,
+    Skeleton,
+    Slider,
+    Space,
+    Statistic,
+    StepItem,
+    Steps,
+    StickyItem,
+    StickyTool,
+    Submenu,
+    Swiper,
+    Switch,
+    Tag,
+    TagInput,
+    Textarea,
+    TimePicker,
+    TimeRangePicker,
+    Timeline,
+    TimelineItem,
+    Tooltip,
+    TooltipLite,
+    Transfer,
+    Tree,
+    TreeSelect,
+    Upload,
+    Watermark,
+  ]
+}
+
+export function getComponentsMap() {
+  return [
+    [Affix, Affix.name, `<${Affix.name}></${Affix.name}`],
+    [Alert, Alert.name, `<${Alert.name}></${Alert.name}`],
+    [Anchor, Anchor.name, `<${Anchor.name}></${Anchor.name}`],
+    [AnchorItem, AnchorItem.name, `<${AnchorItem.name}></${AnchorItem.name}`],
+    [AnchorTarget, AnchorTarget.name, `<${AnchorTarget.name}></${AnchorTarget.name}`],
+    [Aside, Aside.name, `<${Aside.name}></${Aside.name}`],
+    [AutoComplete, AutoComplete.name, `<${AutoComplete.name}></${AutoComplete.name}`],
+    [Avatar, Avatar.name, `<${Avatar.name}></${Avatar.name}`],
+    [AvatarGroup, AvatarGroup.name, `<${AvatarGroup.name}></${AvatarGroup.name}`],
+    [BackTop, BackTop.name, `<${BackTop.name}></${BackTop.name}`],
+    [Badge, Badge.name, `<${Badge.name}></${Badge.name}`],
+    [BaseTable, BaseTable.name, `<${BaseTable.name}></${BaseTable.name}`],
+    [Breadcrumb, Breadcrumb.name, `<${Breadcrumb.name}></${Breadcrumb.name}`],
+    [BreadcrumbItem, BreadcrumbItem.name, `<${BreadcrumbItem.name}></${BreadcrumbItem.name}`],
+    [Button, Button.name, `<${Button.name}></${Button.name}`],
+    [Calendar, Calendar.name, `<${Calendar.name}></${Calendar.name}`],
+    [Card, Card.name, `<${Card.name}></${Card.name}`],
+    [Cascader, Cascader.name, `<${Cascader.name}></${Cascader.name}`],
+    [CheckTag, CheckTag.name, `<${CheckTag.name}></${CheckTag.name}`],
+    [CheckTagGroup, CheckTagGroup.name, `<${CheckTagGroup.name}></${CheckTagGroup.name}`],
+    [Checkbox, Checkbox.name, `<${Checkbox.name}></${Checkbox.name}`],
+    [CheckboxGroup, CheckboxGroup.name, `<${CheckboxGroup.name}></${CheckboxGroup.name}`],
+    [Col, Col.name, `<${Col.name}></${Col.name}`],
+    [Collapse, Collapse.name, `<${Collapse.name}></${Collapse.name}`],
+    [CollapsePanel, CollapsePanel.name, `<${CollapsePanel.name}></${CollapsePanel.name}`],
+    [ColorPicker, ColorPicker.name, `<${ColorPicker.name}></${ColorPicker.name}`],
+    [Comment, Comment.name, `<${Comment.name}></${Comment.name}`],
+    [DatePicker, DatePicker.name, `<${DatePicker.name}></${DatePicker.name}`],
+    [DatePickerPanel, DatePickerPanel.name, `<${DatePickerPanel.name}></${DatePickerPanel.name}`],
+    [DateRangePicker, DateRangePicker.name, `<${DateRangePicker.name}></${DateRangePicker.name}`],
+    [DateRangePickerPanel, DateRangePickerPanel.name, `<${DateRangePickerPanel.name}></${DateRangePickerPanel.name}`],
+    [DescriptionItem, DescriptionItem.name, `<${DescriptionItem.name}></${DescriptionItem.name}`],
+    [Descriptions, Descriptions.name, `<${Descriptions.name}></${Descriptions.name}`],
+    [Dialog, Dialog.name, `<${Dialog.name}></${Dialog.name}`],
+    [Divider, Divider.name, `<${Divider.name}></${Divider.name}`],
+    [Drawer, Drawer.name, `<${Drawer.name}></${Drawer.name}`],
+    [Dropdown, Dropdown.name, `<${Dropdown.name}></${Dropdown.name}`],
+    [DropdownItem, DropdownItem.name, `<${DropdownItem.name}></${DropdownItem.name}`],
+    [DropdownMenu, DropdownMenu.name, `<${DropdownMenu.name}></${DropdownMenu.name}`],
+    [Empty, Empty.name, `<${Empty.name}></${Empty.name}`],
+    [EnhancedTable, EnhancedTable.name, `<${EnhancedTable.name}></${EnhancedTable.name}`],
+    [Footer, Footer.name, `<${Footer.name}></${Footer.name}`],
+    [Form, Form.name, `<${Form.name}></${Form.name}`],
+    [FormItem, FormItem.name, `<${FormItem.name}></${FormItem.name}`],
+    [Guide, Guide.name, `<${Guide.name}></${Guide.name}`],
+    [HeadMenu, HeadMenu.name, `<${HeadMenu.name}></${HeadMenu.name}`],
+    [Header, Header.name, `<${Header.name}></${Header.name}`],
+    [Image, Image.name, `<${Image.name}></${Image.name}`],
+    [ImageViewer, ImageViewer.name, `<${ImageViewer.name}></${ImageViewer.name}`],
+    [Input, Input.name, `<${Input.name}></${Input.name}`],
+    [InputAdornment, InputAdornment.name, `<${InputAdornment.name}></${InputAdornment.name}`],
+    [InputGroup, InputGroup.name, `<${InputGroup.name}></${InputGroup.name}`],
+    [InputNumber, InputNumber.name, `<${InputNumber.name}></${InputNumber.name}`],
+    [Layout, Layout.name, `<${Layout.name}></${Layout.name}`],
+    [Link, Link.name, `<${Link.name}></${Link.name}`],
+    [List, List.name, `<${List.name}></${List.name}`],
+    [ListItem, ListItem.name, `<${ListItem.name}></${ListItem.name}`],
+    [ListItemMeta, ListItemMeta.name, `<${ListItemMeta.name}></${ListItemMeta.name}`],
+    [Loading, Loading.name, `<${Loading.name}></${Loading.name}`],
+    [Menu, Menu.name, `<${Menu.name}></${Menu.name}`],
+    [MenuGroup, MenuGroup.name, `<${MenuGroup.name}></${MenuGroup.name}`],
+    [MenuItem, MenuItem.name, `<${MenuItem.name}></${MenuItem.name}`],
+    [Message, Message.name, `<${Message.name}></${Message.name}`],
+    [Notification, Notification.name, `<${Notification.name}></${Notification.name}`],
+    [Option, Option.name, `<${Option.name}></${Option.name}`],
+    [OptionGroup, OptionGroup.name, `<${OptionGroup.name}></${OptionGroup.name}`],
+    [Pagination, Pagination.name, `<${Pagination.name}></${Pagination.name}`],
+    [PaginationMini, PaginationMini.name, `<${PaginationMini.name}></${PaginationMini.name}`],
+    [Popconfirm, Popconfirm.name, `<${Popconfirm.name}></${Popconfirm.name}`],
+    [Popup, Popup.name, `<${Popup.name}></${Popup.name}`],
+    [PrimaryTable, PrimaryTable.name, `<${PrimaryTable.name}></${PrimaryTable.name}`],
+    [Progress, Progress.name, `<${Progress.name}></${Progress.name}`],
+    [Radio, Radio.name, `<${Radio.name}></${Radio.name}`],
+    [RadioGroup, RadioGroup.name, `<${RadioGroup.name}></${RadioGroup.name}`],
+    [RangeInput, RangeInput.name, `<${RangeInput.name}></${RangeInput.name}`],
+    [RangeInputPopup, RangeInputPopup.name, `<${RangeInputPopup.name}></${RangeInputPopup.name}`],
+    [Rate, Rate.name, `<${Rate.name}></${Rate.name}`],
+    [Row, Row.name, `<${Row.name}></${Row.name}`],
+    [Select, Select.name, `<${Select.name}></${Select.name}`],
+    [SelectInput, SelectInput.name, `<${SelectInput.name}></${SelectInput.name}`],
+    [Skeleton, Skeleton.name, `<${Skeleton.name}></${Skeleton.name}`],
+    [Slider, Slider.name, `<${Slider.name}></${Slider.name}`],
+    [Space, Space.name, `<${Space.name}></${Space.name}`],
+    [Statistic, Statistic.name, `<${Statistic.name}></${Statistic.name}`],
+    [StepItem, StepItem.name, `<${StepItem.name}></${StepItem.name}`],
+    [Steps, Steps.name, `<${Steps.name}></${Steps.name}`],
+    [StickyItem, StickyItem.name, `<${StickyItem.name}></${StickyItem.name}`],
+    [StickyTool, StickyTool.name, `<${StickyTool.name}></${StickyTool.name}`],
+    [Submenu, Submenu.name, `<${Submenu.name}></${Submenu.name}`],
+    [Swiper, Swiper.name, `<${Swiper.name}></${Swiper.name}`],
+    [Switch, Switch.name, `<${Switch.name}></${Switch.name}`],
+    [Tag, Tag.name, `<${Tag.name}></${Tag.name}`],
+    [TagInput, TagInput.name, `<${TagInput.name}></${TagInput.name}`],
+    [Textarea, Textarea.name, `<${Textarea.name}></${Textarea.name}`],
+    [TimePicker, TimePicker.name, `<${TimePicker.name}></${TimePicker.name}`],
+    [TimeRangePicker, TimeRangePicker.name, `<${TimeRangePicker.name}></${TimeRangePicker.name}`],
+    [Timeline, Timeline.name, `<${Timeline.name}></${Timeline.name}`],
+    [TimelineItem, TimelineItem.name, `<${TimelineItem.name}></${TimelineItem.name}`],
+    [Tooltip, Tooltip.name, `<${Tooltip.name}></${Tooltip.name}`],
+    [TooltipLite, TooltipLite.name, `<${TooltipLite.name}></${TooltipLite.name}`],
+    [Transfer, Transfer.name, `<${Transfer.name}></${Transfer.name}`],
+    [Tree, Tree.name, `<${Tree.name}></${Tree.name}`],
+    [TreeSelect, TreeSelect.name, `<${TreeSelect.name}></${TreeSelect.name}`],
+    [Upload, Upload.name, `<${Upload.name}></${Upload.name}`],
+    [Watermark, Watermark.name, `<${Watermark.name}></${Watermark.name}`],
+  ]
+}
